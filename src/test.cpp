bf31939754cfc497b038f91f8e456096dd62e5a7
Adrian Roth <adrianroth10@gmail.com>
2016-11-09 16:07:51 +0100

    compiling test program

---
+++ src/test.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main(int, char**) {
+	cv::VideoCapture vcap;
+	cv::Mat image;
+
+	// This works on a D-Link CDS-932L
+	const std::string videoStreamAddress = "http://<root:ptz-fman40>@<31.208.80.26:80>/video.cgi?.mjpg";
+
+	//open the video stream and make sure it's opened
+	if(!vcap.open(videoStreamAddress)) {
+		std::cout << "Error opening video stream or file" << std::endl;
+		return -1;
+	}
+
+	for(;;) {
+		if(!vcap.read(image)) {
+			std::cout << "No frame" << std::endl;
+			cv::waitKey();
+		}
+		cv::imshow("Output Window", image);
+		if(cv::waitKey(1) >= 0) break;
+	}
+}
