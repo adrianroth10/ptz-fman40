2db1bab084c2c3df0cf21ad8860d73ee21cf812c
Adrian Roth <adrianroth10@gmail.com>
2016-11-29 12:31:25 +0100

    surf almost functioning

--- src/surf.cpp
+++ src/surf.cpp
@@ -1,9 +1,3 @@
-/**
- * @file SURF_Homography
- * @brief SURF detector + descriptor + FLANN Matcher + FindHomography
- * @author A. Huaman
- */
-
 #include <stdio.h>
 
 #include <opencv2/calib3d/calib3d.hpp>
@@ -12,11 +6,12 @@
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/xfeatures2d/nonfree.hpp>
+#include <opencv2/xfeatures2d.hpp>
 
 #include "camera.hpp"
 
 using namespace cv;
+using namespace cv::xfeatures2d;
 
 // Different locations of the data
 #ifdef LINUX
@@ -59,7 +54,7 @@ int main( int argc, char** argv )
 	initUndistortRectifyMap(cameraMatrix,
 			        distortCoefficients,
 			        noArray(),
-			        newCameraMatrix,
+			        cameraMatrix,
 			        img1.size(),
 			        CV_32FC1,
 			        rectmap1,
@@ -68,16 +63,12 @@ int main( int argc, char** argv )
 	      newimg1,
 	      rectmap1,
 	      rectmap2,
-	      INTER_LINEAR,
-	      BORDER_CONSTANT,
-	      Scalar(0, 0, 0, 0));
+	      INTER_LINEAR);
 	remap(img2,
 	      newimg2,
 	      rectmap1,
 	      rectmap2,
-	      INTER_LINEAR,
-	      BORDER_CONSTANT,
-	      Scalar(0, 0, 0, 0));
+	      INTER_LINEAR);
 
 	cvtColor(newimg1, img1_gray, COLOR_BGR2GRAY);
 	cvtColor(newimg2, img2_gray, COLOR_BGR2GRAY);
@@ -87,57 +78,56 @@ int main( int argc, char** argv )
 	//-- Step 1: Detect the keypoints using SURF Detector
 	int minHessian = 400;
 
-	SurfFeatureDetector detector( minHessian );
+	Ptr<SURF> detector = xfeatures2d::SURF::create(minHessian);
 
 	std::vector<KeyPoint> keypoints_object, keypoints_scene;
 
-	detector.detect( img1, keypoints_object );
-	detector.detect( img2, keypoints_scene );
+	detector->detect(img1, keypoints_object);
+	detector->detect(img2, keypoints_scene);
 
 	//-- Step 2: Calculate descriptors (feature vectors)
-	SurfDescriptorExtractor extractor;
 
 	Mat descriptors_object, descriptors_scene;
 
-	extractor.compute( img1, keypoints_object, descriptors_object );
-	extractor.compute( img2, keypoints_scene, descriptors_scene );
+	detector->compute(img1, keypoints_object, descriptors_object);
+	detector->compute(img2, keypoints_scene, descriptors_scene);
 
 	//-- Step 3: Matching descriptor vectors using FLANN matcher
 	FlannBasedMatcher matcher;
 	std::vector< DMatch > matches;
-	matcher.match( descriptors_object, descriptors_scene, matches );
+	matcher.match(descriptors_object, descriptors_scene, matches);
 
 	double max_dist = 0; double min_dist = 100;
 
 	//-- Quick calculation of max and min distances between keypoints
-	for( int i = 0; i < descriptors_object.rows; i++ )
-	{ double dist = matches[i].distance;
-		if( dist < min_dist ) min_dist = dist;
-		if( dist > max_dist ) max_dist = dist;
+	for (int i = 0; i < descriptors_object.rows; i++) {
+		double dist = matches[i].distance;
+		if (dist < min_dist) min_dist = dist;
+		if (dist > max_dist) max_dist = dist;
 	}
 
-	printf("-- Max dist : %f \n", max_dist );
-	printf("-- Min dist : %f \n", min_dist );
+	printf("-- Max dist : %f \n", max_dist);
+	printf("-- Min dist : %f \n", min_dist);
 
 	//-- Draw only "good" matches (i.e. whose distance is less than 3*min_dist )
 	std::vector< DMatch > good_matches;
 
-	for( int i = 0; i < descriptors_object.rows; i++ )
-	{ if( matches[i].distance < 3*min_dist )
-		{ good_matches.push_back( matches[i]); }
+	for (int i = 0; i < descriptors_object.rows; i++) {
+		if (matches[i].distance < 3*min_dist) {
+			good_matches.push_back(matches[i]);
+		}
 	}
 
 	Mat img_matches;
-	drawMatches( img1, keypoints_object, img2, keypoints_scene,
+	drawMatches(img1, keypoints_object, img2, keypoints_scene,
 			good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
-			std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
-
+			std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
 
 	//-- Localize the object from img_1 in img_2
 	std::vector<Point2f> obj;
 	std::vector<Point2f> scene;
 
-	for( size_t i = 0; i < good_matches.size(); i++ )
+	for ( size_t i = 0; i < good_matches.size(); i++ )
 	{
 		//-- Get the keypoints from the good matches
 		obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
@@ -145,11 +135,22 @@ int main( int argc, char** argv )
 	}
 
 	Mat H = findHomography( obj, scene, CV_RANSAC );
+	//Mat trans(3, 3, 1);
+	double xtrans=2000.0;
+	Mat trans=(Mat1d(3,3) << 1.0,0.0,xtrans,0.0,1.0,0.0,0.0,0.0,1.0);
+	Mat res=trans*H;
+	Mat out(img1.size().height, img1.size().width + 1000, 3);
+	warpPerspective(img1, out, res, out.size());
+	const string test = "Test";
+	namedWindow(test, WINDOW_NORMAL);
+	imshow( test, out );
 
 	//-- Get the corners from the image_1 ( the object to be "detected" )
 	std::vector<Point2f> obj_corners(4);
-	obj_corners[0] = Point(0,0); obj_corners[1] = Point( img1.cols, 0 );
-	obj_corners[2] = Point( img1.cols, img1.rows ); obj_corners[3] = Point( 0, img1.rows );
+	obj_corners[0] = Point(0,0);
+	obj_corners[1] = Point( img1.cols, 0 );
+	obj_corners[2] = Point( img1.cols, img1.rows );
+	obj_corners[3] = Point( 0, img1.rows );
 	std::vector<Point2f> scene_corners(4);
 
 	perspectiveTransform( obj_corners, scene_corners, H);
@@ -157,15 +158,18 @@ int main( int argc, char** argv )
 
 	//-- Draw lines between the corners (the mapped object in the scene - image_2 )
 	Point2f offset( (float)img1.cols, 0);
-	line( img_matches, scene_corners[0] + offset, scene_corners[1] + offset, Scalar(0, 255, 0), 4 );
-	line( img_matches, scene_corners[1] + offset, scene_corners[2] + offset, Scalar( 0, 255, 0), 4 );
-	line( img_matches, scene_corners[2] + offset, scene_corners[3] + offset, Scalar( 0, 255, 0), 4 );
-	line( img_matches, scene_corners[3] + offset, scene_corners[0] + offset, Scalar( 0, 255, 0), 4 );
+	line(img_matches, scene_corners[0] + offset, scene_corners[1] + offset, Scalar(0, 255, 0), 4);
+	line(img_matches, scene_corners[1] + offset, scene_corners[2] + offset, Scalar(0, 255, 0), 4);
+	line(img_matches, scene_corners[2] + offset, scene_corners[3] + offset, Scalar(0, 255, 0), 4);
+	line(img_matches, scene_corners[3] + offset, scene_corners[0] + offset, Scalar(0, 255, 0), 4);
 
 	//-- Show detected matches
-	imshow( "Good Matches & Object detection", img_matches );
+	const string features = "Good Matches & Object detection";
+	namedWindow(features, WINDOW_NORMAL);
+	imshow( features, img_matches );
 
-	waitKey(0);
+	while (waitKey(0) != '\n');
+
 
 	return 0;
 }
