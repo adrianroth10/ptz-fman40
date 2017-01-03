cdc49fdbf301ac3b3ef0d4beb9aaf5bde5746c97
Filip <filip.johannesson@gmail.com>
2017-01-03 15:46:28 +0100

    tried a new thing_1

--- src/VirtualCamera.cpp
+++ src/VirtualCamera.cpp
@@ -16,7 +16,7 @@ VirtualCamera::VirtualCamera()
 
 Mat VirtualCamera::updateView(char key) 
 {
-	double orgtilt=-0.47179832679;
+	//double orgtilt=-0.47179832679;
 	//std::cout<<M_PI<<std::endl;
 	if (key == 'w')
 	{
@@ -31,23 +31,23 @@ Mat VirtualCamera::updateView(char key)
 	}
 	else if (key == 'd') {
 		//Perspective.at<double>(2, 0) += 0.00005;
-		Perspective.at<double>(0, 2) -= 0.05*cos(tiltangle+orgtilt);
+		//Perspective.at<double>(0, 2) -= 0.05*cos(tiltangle+orgtilt);
 		panangle+=M_PI/100.0;
 	}
 	else if (key == 'a') {
 		//Perspective.at<double>(2, 0) -= 0.00005;
-		Perspective.at<double>(0, 2) += 0.05*cos(tiltangle+orgtilt);
+		//Perspective.at<double>(0, 2) += 0.05*cos(tiltangle+orgtilt);
 		panangle-=M_PI/100.0;
 	}
 	else if (key == 'r') {
 		Perspective.at<double>(0, 0) += 0.1;
 		Perspective.at<double>(1, 1) += 0.1;
-		Perspective.at<double>(2, 2) += 0.1;
+		//Perspective.at<double>(2, 2) += 0.1;
 	}
 	else if (key == 't') {
 		Perspective.at<double>(0, 0) -= 0.1;
 		Perspective.at<double>(1, 1) -= 0.1;
-		Perspective.at<double>(2, 2) -= 0.1;
+		//Perspective.at<double>(2, 2) -= 0.1;
 	}
 	
 	Mat RotTilt = (Mat1d(3,3)<<1.0,0.0,0.0, 0.0,cos(tiltangle),-sin(tiltangle), 0.0,sin(tiltangle),cos(tiltangle));
